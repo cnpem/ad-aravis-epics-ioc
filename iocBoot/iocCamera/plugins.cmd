887692a6c7608c50cf16702abd564c8fd20f9c93
Henrique F. Simoes <henrique.simoes@lnls.br>
2025-04-16 12:12:22 -0300

    plugins: define default value for shared variables once
    Instead of redefining their default value in each usage, let's provide a
    fallback value once and use the variable everywhere directly. This is
    way easier to ensure the default value holds, and cleaner to later
    change it.
    
    Waveform-related parameters have been kept only in their usage, since
    they shouldn't have any other user in the future.

--- iocBoot/iocCamera/plugins.cmd
+++ iocBoot/iocCamera/plugins.cmd
@@ -49,51 +49,56 @@
 # The maximum number of threads for plugins which can run in multiple threads.
 # Defaults to 4.
 
+epicsEnvSet("QSIZE", "$(QSIZE=20)")
+epicsEnvSet("QSIZE_HDF5", "$(QSIZE_HDF5=50)")
+epicsEnvSet("NCHANS", "$(NCHANS=2048)")
+epicsEnvSet("MAX_THREADS", "$(MAX_THREADS=4)")
+
 # Create Channel Access conversion plugin
-NDStdArraysConfigure("Image1", $(QSIZE=20), 0, $(PORT), 0, 0, 0, 0)
+NDStdArraysConfigure("Image1", $(QSIZE), 0, $(PORT), 0, 0, 0, 0)
 dbLoadRecords("NDStdArrays.template", "P=$(PREFIX), R=image1:, PORT=Image1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT), TYPE=$(IMAGE_ASYN_TYPE=Int16), FTVL=$(IMAGE_WAVEFORM_TYPE=SHORT), NELEMENTS=$(MAX_IMAGE_PIXELS)")
 
 # Create 3 ROI plugins
-NDROIConfigure("ROI1", $(QSIZE=20), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
+NDROIConfigure("ROI1", $(QSIZE), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS))
 dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROI1:, PORT=ROI1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
 
-NDROIConfigure("ROI2", $(QSIZE=20), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
+NDROIConfigure("ROI2", $(QSIZE), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS))
 dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROI2:, PORT=ROI2, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
 
-NDROIConfigure("ROI3", $(QSIZE=20), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
+NDROIConfigure("ROI3", $(QSIZE), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS))
 dbLoadRecords("NDROI.template", "P=$(PREFIX), R=ROI3:, PORT=ROI3, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
 
 # Create 3 statistics plugins with time series
-NDStatsConfigure("STATS1", $(QSIZE=20), 0, "ROI1", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
-dbLoadRecords("NDStats.template", "P=$(PREFIX), R=Stats1:, PORT=STATS1, ADDR=0, TIMEOUT=1, HIST_SIZE=256, XSIZE=$(MAX_IMAGE_WIDTH), YSIZE=$(MAX_IMAGE_HEIGHT), NCHANS=$(NCHANS=2048), NDARRAY_PORT=ROI1")
-NDTimeSeriesConfigure("STATS1_TS", $(QSIZE=20), 0, "STATS1", 1, 23, 0, 0, 0, 0)
-dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template", "P=$(PREFIX), R=Stats1:TS:, PORT=STATS1_TS, ADDR=0, TIMEOUT=1, NDARRAY_PORT=STATS1, NDARRAY_ADDR=1, NCHANS=$(NCHANS=2048), ENABLED=1")
+NDStatsConfigure("STATS1", $(QSIZE), 0, "ROI1", 0, 0, 0, 0, 0, $(MAX_THREADS))
+dbLoadRecords("NDStats.template", "P=$(PREFIX), R=Stats1:, PORT=STATS1, ADDR=0, TIMEOUT=1, HIST_SIZE=256, XSIZE=$(MAX_IMAGE_WIDTH), YSIZE=$(MAX_IMAGE_HEIGHT), NCHANS=$(NCHANS), NDARRAY_PORT=ROI1")
+NDTimeSeriesConfigure("STATS1_TS", $(QSIZE), 0, "STATS1", 1, 23, 0, 0, 0, 0)
+dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template", "P=$(PREFIX), R=Stats1:TS:, PORT=STATS1_TS, ADDR=0, TIMEOUT=1, NDARRAY_PORT=STATS1, NDARRAY_ADDR=1, NCHANS=$(NCHANS), ENABLED=1")
 
-NDStatsConfigure("STATS2", $(QSIZE=20), 0, "ROI2", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
-dbLoadRecords("NDStats.template", "P=$(PREFIX), R=Stats2:, PORT=STATS2, ADDR=0, TIMEOUT=1, HIST_SIZE=256, XSIZE=$(MAX_IMAGE_WIDTH), YSIZE=$(MAX_IMAGE_HEIGHT), NCHANS=$(NCHANS=2048), NDARRAY_PORT=ROI2")
-NDTimeSeriesConfigure("STATS2_TS", $(QSIZE=20), 0, "STATS2", 1, 23, 0, 0, 0, 0)
-dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template", "P=$(PREFIX), R=Stats2:TS:, PORT=STATS2_TS, ADDR=0, TIMEOUT=1, NDARRAY_PORT=STATS2, NDARRAY_ADDR=1, NCHANS=$(NCHANS=2048), ENABLED=1")
+NDStatsConfigure("STATS2", $(QSIZE), 0, "ROI2", 0, 0, 0, 0, 0, $(MAX_THREADS))
+dbLoadRecords("NDStats.template", "P=$(PREFIX), R=Stats2:, PORT=STATS2, ADDR=0, TIMEOUT=1, HIST_SIZE=256, XSIZE=$(MAX_IMAGE_WIDTH), YSIZE=$(MAX_IMAGE_HEIGHT), NCHANS=$(NCHANS), NDARRAY_PORT=ROI2")
+NDTimeSeriesConfigure("STATS2_TS", $(QSIZE), 0, "STATS2", 1, 23, 0, 0, 0, 0)
+dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template", "P=$(PREFIX), R=Stats2:TS:, PORT=STATS2_TS, ADDR=0, TIMEOUT=1, NDARRAY_PORT=STATS2, NDARRAY_ADDR=1, NCHANS=$(NCHANS), ENABLED=1")
 
-NDStatsConfigure("STATS3", $(QSIZE=20), 0, "ROI3", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
-dbLoadRecords("NDStats.template", "P=$(PREFIX), R=Stats3:, PORT=STATS3, ADDR=0, TIMEOUT=1, HIST_SIZE=256, XSIZE=$(MAX_IMAGE_WIDTH), YSIZE=$(MAX_IMAGE_HEIGHT), NCHANS=$(NCHANS=2048), NDARRAY_PORT=ROI3")
-NDTimeSeriesConfigure("STATS3_TS", $(QSIZE=20), 0, "STATS3", 1, 23, 0, 0, 0, 0)
-dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template", "P=$(PREFIX), R=Stats3:TS:, PORT=STATS3_TS, ADDR=0, TIMEOUT=1, NDARRAY_PORT=STATS3, NDARRAY_ADDR=1, NCHANS=$(NCHANS=2048), ENABLED=1")
+NDStatsConfigure("STATS3", $(QSIZE), 0, "ROI3", 0, 0, 0, 0, 0, $(MAX_THREADS))
+dbLoadRecords("NDStats.template", "P=$(PREFIX), R=Stats3:, PORT=STATS3, ADDR=0, TIMEOUT=1, HIST_SIZE=256, XSIZE=$(MAX_IMAGE_WIDTH), YSIZE=$(MAX_IMAGE_HEIGHT), NCHANS=$(NCHANS), NDARRAY_PORT=ROI3")
+NDTimeSeriesConfigure("STATS3_TS", $(QSIZE), 0, "STATS3", 1, 23, 0, 0, 0, 0)
+dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template", "P=$(PREFIX), R=Stats3:TS:, PORT=STATS3_TS, ADDR=0, TIMEOUT=1, NDARRAY_PORT=STATS3, NDARRAY_ADDR=1, NCHANS=$(NCHANS), ENABLED=1")
 
 # Create ROIStat plugin with 3 ROIs
-NDROIStatConfigure("ROISTAT1", $(QSIZE=20), 0, "$(PORT)", 0, 3, 0, 0, 0, 0, $(MAX_THREADS=4))
-dbLoadRecords("NDROIStat.template",  "P=$(PREFIX), R=ROIStat1:,   PORT=ROISTAT1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT), NCHANS=$(NCHANS=2048)")
-dbLoadRecords("NDROIStatN.template", "P=$(PREFIX), R=ROIStat1:1:, PORT=ROISTAT1, ADDR=0, TIMEOUT=1, NCHANS=$(NCHANS=2048)")
-dbLoadRecords("NDROIStatN.template", "P=$(PREFIX), R=ROIStat1:2:, PORT=ROISTAT1, ADDR=1, TIMEOUT=1, NCHANS=$(NCHANS=2048)")
-dbLoadRecords("NDROIStatN.template", "P=$(PREFIX), R=ROIStat1:3:, PORT=ROISTAT1, ADDR=2, TIMEOUT=1, NCHANS=$(NCHANS=2048)")
+NDROIStatConfigure("ROISTAT1", $(QSIZE), 0, "$(PORT)", 0, 3, 0, 0, 0, 0, $(MAX_THREADS))
+dbLoadRecords("NDROIStat.template",  "P=$(PREFIX), R=ROIStat1:,   PORT=ROISTAT1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT), NCHANS=$(NCHANS)")
+dbLoadRecords("NDROIStatN.template", "P=$(PREFIX), R=ROIStat1:1:, PORT=ROISTAT1, ADDR=0, TIMEOUT=1, NCHANS=$(NCHANS)")
+dbLoadRecords("NDROIStatN.template", "P=$(PREFIX), R=ROIStat1:2:, PORT=ROISTAT1, ADDR=1, TIMEOUT=1, NCHANS=$(NCHANS)")
+dbLoadRecords("NDROIStatN.template", "P=$(PREFIX), R=ROIStat1:3:, PORT=ROISTAT1, ADDR=2, TIMEOUT=1, NCHANS=$(NCHANS)")
 
 # Create a transform plugin
-NDTransformConfigure("TRANSF1", $(QSIZE=20), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
+NDTransformConfigure("TRANSF1", $(QSIZE), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS))
 dbLoadRecords("$(ADCORE)/db/NDTransform.template", "P=$(PREFIX), R=Trans1:, PORT=TRANSF1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
 
 # Configure HDF5 file format plugin
-NDFileHDF5Configure("FileHDF1", $(QSIZE_HDF5=50), 0, "$(PORT)", 0)
+NDFileHDF5Configure("FileHDF1", $(QSIZE_HDF5), 0, "$(PORT)", 0)
 dbLoadRecords("NDFileHDF5.template", "P=$(PREFIX), R=HDF1:, PORT=FileHDF1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
 
 # Create color conversion plugin
-NDColorConvertConfigure("CC1", $(QSIZE=20), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS=4))
+NDColorConvertConfigure("CC1", $(QSIZE), 0, "$(PORT)", 0, 0, 0, 0, 0, $(MAX_THREADS))
 dbLoadRecords("NDColorConvert.template", "P=$(PREFIX), R=CC1:, PORT=CC1, ADDR=0, TIMEOUT=1, NDARRAY_PORT=$(PORT)")
